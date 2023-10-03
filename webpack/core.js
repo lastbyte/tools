55d07cabe7a357d102d02209414de6a32e1ae549
nishant <nishant.9c9@gmail.com>
2023-10-03 10:55:34 +0530

    gh-pages changes

--- webpack/core.js
+++ webpack/core.js
@@ -41,7 +41,7 @@ module.exports = {
         }
     },
     output: {
-        path: path.resolve(__dirname, '../public'),
+        path: path.resolve(__dirname, '../build'),
         filename: "[name].js",
     }
 }
